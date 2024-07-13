b6ade4df19a31619de9c56cda59c59e960a9c73c
George-Hampson <georgehampson.gh@gmail.com>
2024-07-13 20:12:44 +0100

    Visual updates + version + linting issues

--- app/app.py
+++ app/app.py
@@ -2,13 +2,11 @@ from flask import Flask, render_template
 import os
 from datetime import datetime
 
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
-    # Get GitHub run number from environment variable, defaulting to 'Local'
+    # Get GitHub run number and commit count from environment variables
     github_run_number = os.getenv('GITHUB_RUN_NUMBER', 'Local')
     commit_count = os.getenv('GITHUB_COMMIT_COUNT', '0')
 
@@ -16,10 +14,7 @@ def index():
     current_date = datetime.now().strftime('%Y-%m-%d')
 
     # Render the template 'index.html' and pass variables to it
-    return render_template('index.html', version=github_run_number,
-                           commit_count=commit_count,
-                           current_date=current_date)
-
+    return render_template('index.html', version=github_run_number, commit_count=commit_count, current_date=current_date)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
