0a14e0afbae941da2962b08f436822091c8c858a
George-Hampson <georgehampson.gh@gmail.com>
2024-07-13 21:09:20 +0100

    Visual updates + version + linting issues

--- app/app.py
+++ app/app.py
@@ -5,47 +5,27 @@ import subprocess
 app = Flask(__name__)
 
 
-def get_commit_count():
-    try:
-        result = subprocess.run(['git', 'rev-list', '--count', 'HEAD'],
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        if result.returncode == 0:
-            return result.stdout.decode('utf-8').strip()
-        else:
-            print("Error in get_commit_count: ", result.stderr.decode('utf-8'))
-            return "0"
-    except Exception as e:
-        print("Exception in get_commit_count: ", e)
-        return "0"
-
-
-def get_shortlog():
+def get_git_shortlog():
     try:
         result = subprocess.run(['git', 'shortlog', '-s'],
                                 stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        if result.returncode == 0:
-            shortlog = result.stdout.decode('utf-8').strip().split('\n')
-            shortlog_list = [line.split('\t') for line in shortlog if line]
-            print("Shortlog List: ", shortlog_list)  # Debugging statement
-            return shortlog_list
-        else:
-            print("Error in get_shortlog: ", result.stderr.decode('utf-8'))
-            return []
+                                stderr=subprocess.PIPE, text=True)
+        if result.returncode != 0:
+            raise Exception(result.stderr)
+        shortlog = result.stdout.strip().split('\n')
+        return [line.split('\t') for line in shortlog]
     except Exception as e:
-        print("Exception in get_shortlog: ", e)
         return []
 
 
 @app.route('/')
 def index():
-    commit_count = get_commit_count()
-    shortlog_list = get_shortlog()
-    print("Commit Count: ", commit_count)  # Debugging statement
-    print("Shortlog List at Index: ", shortlog_list)  # Debugging statement
-    return render_template('index.html', version=commit_count,
-                           shortlog=shortlog_list)
+    github_run_number = os.getenv('GITHUB_RUN_NUMBER', 'Local')
+    commit_count = os.getenv('GITHUB_COMMIT_COUNT', 'Unknown')
+    shortlog_data = get_git_shortlog()
+    return render_template('index.html', version=github_run_number,
+                           commit_count=commit_count,
+                           shortlog_data=shortlog_data)
 
 
 if __name__ == '__main__':
