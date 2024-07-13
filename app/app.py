f19b161c1e817d25a799cc4c20ff8780572ee9d4
George-Hampson <georgehampson.gh@gmail.com>
2024-07-13 18:39:55 +0100

    Add config file for flake8, prevents errors when linting packages

--- app/app.py
+++ app/app.py
@@ -5,12 +5,15 @@ app = Flask(__name__)
 
 # Define a route for the root URL '/'
 @app.route('/')
+
+
 def index():
     # Get GitHub run number from environment variable, defaulting to 'Local'
     github_run_number = os.getenv('GITHUB_RUN_NUMBER', 'Local')
 
     # Render the template 'index.html' and pass variables to it
     return render_template('index.html', version=github_run_number)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
