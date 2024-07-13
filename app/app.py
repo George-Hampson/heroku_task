5b9c45fe9a0b64e53f93894d60dd20f7f401545e
George-Hampson <georgehampson.gh@gmail.com>
2024-07-13 21:29:47 +0100

    Visual updates + version + linting issues

--- app/app.py
+++ app/app.py
@@ -3,12 +3,13 @@ import os
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    # Render the template 'index.html'
-    return render_template('index.html')
+    # Get the version from environment variable
+    version = os.getenv('VERSION', '0.1.0')
 
+    # Render the template 'index.html' and pass variables to it
+    return render_template('index.html', version=version)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
