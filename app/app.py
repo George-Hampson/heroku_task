0d6cf3d546aae3114c5b2abb7af2b5d79593380c
George-Hampson <georgehampson.gh@gmail.com>
2024-07-13 20:33:59 +0100

    Visual updates + version + linting issues

--- app/app.py
+++ app/app.py
@@ -1,16 +1,31 @@
 from flask import Flask, render_template
 import os
+import subprocess
 
 app = Flask(__name__)
 
 
+def get_commit_count():
+    result = subprocess.run(['git', 'rev-list', '--count', 'HEAD'],
+                            stdout=subprocess.PIPE)
+    return result.stdout.decode('utf-8').strip()
+
+
+def get_shortlog():
+    result = subprocess.run(['git', 'shortlog', '-s'],
+                            stdout=subprocess.PIPE)
+    shortlog = result.stdout.decode('utf-8').strip().split('\n')
+    shortlog_list = [line.split('\t') for line in shortlog if line]
+    return shortlog_list
+
+
 @app.route('/')
 def index():
-    # Get GitHub commit count from environment variable, defaulting to '0'
-    commit_count = os.getenv('GITHUB_COMMIT_COUNT', '0')
+    commit_count = get_commit_count()
+    shortlog_list = get_shortlog()
 
-    # Render the template 'index.html' and pass variables to it
-    return render_template('index.html', version=commit_count)
+    return render_template('index.html', version=commit_count,
+                           shortlog=shortlog_list)
 
 
 if __name__ == '__main__':
